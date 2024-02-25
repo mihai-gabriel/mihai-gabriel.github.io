c0b8c46b76077590808c4f3b357484be6897c0bc
Mihai Gabriel <17096004+mihai-gabriel@users.noreply.github.com>
2024-02-25 12:04:16 +0200

    add favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,6 @@
   import '../app.css';
 </script>
 
-<div in:fade={{ delay: 400, duration: 600, easing: quartInOut }} class="font-space h-lvh w-full">
+<div class="h-lvh w-full font-space">
   <slot />
 </div>
