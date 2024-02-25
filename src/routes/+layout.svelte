ec99d3741efd8b30aca715b68dc52e645d226e88
Mihai Gabriel <17096004+mihai-gabriel@users.noreply.github.com>
2024-02-25 03:12:30 +0200

    Add transitions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,6 @@
   import '../app.css';
 </script>
 
-<div class="font-space h-lvh w-full">
+<div in:fade={{ delay: 400, duration: 600, easing: quartInOut }} class="font-space h-lvh w-full">
   <slot />
 </div>
